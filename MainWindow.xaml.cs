ebd89c2fb4924634a969872a5c92ccb96cef9e87
NetkoNefarious <jmagazin5@gmail.com>
2018-03-07 15:29:39 +0100

    Added buttons, score and timer to the program (non-functioning)

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Timers;
+using System.Windows.Threading;
 
 namespace BocceVoloCounter
 {
@@ -23,6 +23,29 @@ namespace BocceVoloCounter
         public MainWindow()
         {
             InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+            timer.Tick += TimerTick;
+            timer.Start();
+        }
+
+        void TimerTick(object sender, EventArgs e)
+        {
+            int seconds = int.Parse(timerBox.Text.Substring(timerBox.Text.Length - 2, 2)) - 1;
+            timerBox.Text = (new TimeSpan(0, 0, seconds)).ToString(@"mm\:ss");
+        }
+
+        private void Button_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch(e.Key)
+            {
+                case Key.Space:
+                    scoreBox.Text = "1/1";
+                    break;
+            }
+            Keyboard.Focus(hitButton);
         }
     }
 }
